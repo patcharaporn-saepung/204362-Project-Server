45c6f5425114b7af0a85558cc427e3c32ae60dc6
callmepiggypim <patcharaporn_saepung@cmu.ac.th>
2020-03-26 10:55:32 +0700

    Create news model

---
+++ Models/News.cs
@@ -0,0 +1,39 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MheanMaa.Models
+{
+    public class News
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Accepted { get; set; }
+
+        public string Acceptor { get; set; }
+
+        public string Writer { get; set; }
+
+        public string ImgPath { get; set; }
+
+        public int DeptNo { get; set; }
+    }
+    public class NewsList
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Writer { get; set; }
+
+        public bool Accepted { get; set; }
+    }
+}
